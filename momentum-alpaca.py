b229c7ad34a7a83e085e760cda7214952011d87d
Nick Yeager <nick@quoteapro.com>
2020-09-25 06:39:16 -0700

    change market open time to 7 minutes, instead of 14

--- momentum-alpaca.py
+++ momentum-alpaca.py
@@ -447,7 +447,7 @@ if __name__ == "__main__":
     # Wait until just before we might want to trade
     current_dt = datetime.today().astimezone(nyc)
     since_market_open = current_dt - market_open
-    while since_market_open.seconds // 60 <= 14:
+    while since_market_open.seconds // 60 <= 7:
         time.sleep(1)
         since_market_open = current_dt - market_open
 
